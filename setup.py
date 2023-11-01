dea7c36433398266fda757185b9da3d053997c73
tung.tq <tung.tq@teko.vn>
2023-11-01 14:45:48 +0700

    Reformat

--- setup.py
+++ setup.py
@@ -1,33 +1,33 @@
 import setuptools
 
-with open("README.md", "r", encoding = "utf-8") as fh:
+with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name = "memproxy",
-    version = "0.1.0",
+    name="memproxy",
+    version="0.1.0",
     license="MIT",
     keywords=["Redis", "key-value store", "database"],
 
-    author = "quangtung97",
-    author_email = "quangtung29121997@gmail.com",
+    author="quangtung97",
+    author_email="quangtung29121997@gmail.com",
 
-    description = "A library for strong consistent caching",
-    long_description = long_description,
-    long_description_content_type = "text/markdown",
+    description="A library for strong consistent caching",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
 
-    url = "https://github.com/QuangTung97/memproxy-py",
-    project_urls = {
+    url="https://github.com/QuangTung97/memproxy-py",
+    project_urls={
         "Changes": "https://github.com/QuangTung97/memproxy-py/releases",
         "Code": "https://github.com/QuangTung97/memproxy-py",
         "Issue Tracker": "https://github.com/QuangTung97/memproxy-py/issues",
     },
-    classifiers = [
+    classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    package_dir = {"": "src"},
-    packages = setuptools.find_packages(where="src"),
-    python_requires = ">=3.8"
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.8"
 )
