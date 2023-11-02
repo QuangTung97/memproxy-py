5a3a8ad641eac0d0332d3b637f6bb9bf56583c8f
tung.tq <tung.tq@teko.vn>
2023-11-02 13:54:38 +0700

    Lower Session

--- memproxy/session.py
+++ memproxy/session.py
@@ -1,24 +1,50 @@
-from typing import List, Callable
+from __future__ import annotations
+
+from typing import List, Callable, Optional
 
 NextCallFunc = Callable[[], None]
 
 
 class Session:
     _next_calls: List[NextCallFunc]
+    _lower: Optional[Session]
+    _higher: Optional[Session]
+    _is_dirty: bool
 
     def __init__(self):
         self._next_calls = []
+        self._lower = None
+        self._higher = None
+        self._is_dirty = False
 
     def add_next_call(self, fn: NextCallFunc) -> None:
         self._next_calls.append(fn)
+        self._set_dirty_recursive()
 
     def execute(self) -> None:
+        higher = self._higher
+        if higher and higher._is_dirty:
+            higher.execute()
+
         while True:
             call_list = self._next_calls
             self._next_calls = []
+            self._is_dirty = False
 
             if len(call_list) == 0:
                 return
 
             for fn in call_list:
                 fn()
+
+    def _set_dirty_recursive(self):
+        s = self
+        while s and not s._is_dirty:
+            s._is_dirty = True
+            s = s._lower
+
+    def get_lower(self) -> Session:
+        if self._lower is None:
+            self._lower = Session()
+            self._lower._higher = self
+        return self._lower
--- test/test_session.py
+++ test/test_session.py
@@ -42,3 +42,94 @@ class TestSession(unittest.TestCase):
         sess.execute()
 
         self.assertEqual([11, 12], calls)
+
+    def test_lower_session(self) -> None:
+        sess = Session()
+        lower = sess.get_lower()
+
+        self.assertTrue(lower == sess.get_lower())
+        self.assertTrue(sess != sess.get_lower())
+
+        calls: List[int] = []
+
+        lower.add_next_call(lambda: calls.append(31))
+        lower.add_next_call(lambda: calls.append(32))
+
+        sess.add_next_call(lambda: calls.append(21))
+        sess.add_next_call(lambda: calls.append(22))
+
+        lower.execute()
+
+        self.assertEqual([21, 22, 31, 32], calls)
+
+    def test_multi_levels(self) -> None:
+        sess = Session()
+        lower = sess.get_lower()
+        lower2 = lower.get_lower()
+
+        calls: List[int] = []
+
+        lower2.add_next_call(lambda: calls.append(41))
+
+        lower.add_next_call(lambda: calls.append(31))
+        lower.add_next_call(lambda: calls.append(32))
+
+        sess.add_next_call(lambda: calls.append(21))
+        sess.add_next_call(lambda: calls.append(22))
+
+        lower2.execute()
+
+        self.assertEqual([21, 22, 31, 32, 41], calls)
+
+    def test_execute_at_middle(self) -> None:
+        sess = Session()
+        lower = sess.get_lower()
+        lower2 = lower.get_lower()
+
+        calls: List[int] = []
+
+        lower2.add_next_call(lambda: calls.append(41))
+        lower2.add_next_call(lambda: calls.append(42))
+
+        lower.add_next_call(lambda: calls.append(31))
+        lower.add_next_call(lambda: calls.append(32))
+
+        sess.add_next_call(lambda: calls.append(21))
+        sess.add_next_call(lambda: calls.append(22))
+
+        lower.execute()
+
+        self.assertEqual([21, 22, 31, 32], calls)
+
+        calls = []
+        lower.execute()
+        self.assertEqual([], calls)
+
+        calls = []
+        lower2.execute()
+        self.assertEqual([41, 42], calls)
+
+    def test_multi_levels__add_inside(self) -> None:
+        sess = Session()
+        lower = sess.get_lower()
+        lower2 = lower.get_lower()
+
+        calls: List[int] = []
+
+        def lower2_fn():
+            calls.append(41)
+            lower2.add_next_call(lambda: calls.append(42))
+
+        lower2.add_next_call(lower2_fn)
+
+        def lower_fn():
+            calls.append(31)
+            lower.add_next_call(lambda: calls.append(32))
+
+        lower.add_next_call(lower_fn)
+
+        sess.add_next_call(lambda: calls.append(21))
+
+        lower2.execute()
+
+        self.assertEqual([21, 31, 32, 41, 42], calls)
