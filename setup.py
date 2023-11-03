53bef436d50a741cc7106601b34bd15390c181d1
tung.tq <tung.tq@teko.vn>
2023-11-03 15:00:41 +0700

    Update version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="memproxy",
-    version="0.1.0",
+    version="0.2.0",
     license="MIT",
     keywords=["Redis", "key-value store", "caching"],
 
