969be747a3f499c0799460d9dcbf09ae2a7021ff
tung.tq <tung.tq@teko.vn>
2023-11-06 19:06:10 +0700

    Add Stats

--- memproxy/proxy/stats.py
+++ memproxy/proxy/stats.py
@@ -1,11 +1,12 @@
-import datetime
 import logging
 import random
 import threading
 import time
-from typing import Optional, Dict, List, Set
+from typing import Optional, Dict, List, Set, Callable
 
 import redis
+
+MemLogger = Callable[[int, float], None]
 
 
 class _ServerState:
@@ -30,15 +31,20 @@ class _ServerState:
         d = random.randint(sleep_min, sleep_max)
         self.next_wake_up = time.time() + float(d)
 
-    def get_mem_usage(self):
+    def get_mem_usage(self, server_id, mem_logger: MemLogger):
         try:
             usage = self.client.info('memory').get('used_memory')
-            print("GET USAGE =", usage, datetime.datetime.now())
-            self.mem = usage
+            if usage:
+                self.mem = usage
+                mem_logger(server_id, usage)
         except Exception as e:
             self.mem = None
             self.error = str(e)
             logging.error(f'Server Stats error. {str(e)}')
+
+
+def _empty_logger(_server_id: int, _mem: float):
+    pass
 
 
 class ServerStats:
@@ -47,6 +53,7 @@ class ServerStats:
 
     _sleep_min: int
     _sleep_max: int
+    _mem_logger: MemLogger
 
     _mut: threading.Lock
     _notified: Set[int]
@@ -55,9 +62,14 @@ class ServerStats:
 
     _finished: threading.Semaphore
 
-    def __init__(self, clients: Dict[int, redis.Redis], sleep_min: int = 150, sleep_max: int = 300):
+    def __init__(
+            self, clients: Dict[int, redis.Redis],
+            sleep_min: int = 150, sleep_max: int = 300,
+            mem_logger: MemLogger = _empty_logger,
+    ):
         self._sleep_min = sleep_min
         self._sleep_max = sleep_max
+        self._mem_logger = mem_logger
 
         self._mut = threading.Lock()
         self._notified = set()
@@ -76,7 +88,7 @@ class ServerStats:
             state = _ServerState(r=r)
 
             self._states[server_id] = state
-            state.get_mem_usage()
+            state.get_mem_usage(server_id, self._mem_logger)
             state.compute_next_wake_up(sleep_min=self._sleep_min, sleep_max=self._sleep_max)
 
         threading.Thread(target=self._run, daemon=True).start()
@@ -93,42 +105,47 @@ class ServerStats:
 
         return min_wake_up
 
-    def _clear_notified(self):
+    def _notify_timeout(self) -> List[int]:
+        timeout_servers: List[int] = []
+
         now = time.time()
         for server_id in self._servers:
             state = self._states[server_id]
             if state.next_wake_up <= now:
                 state.notified = False
-                self._notified.add(server_id)
+                timeout_servers.append(server_id)
 
-    def _notify_servers(self, servers: List[int]):
+        return timeout_servers
+
+    def _notify_servers(self, servers: List[int], is_timeout: bool):
         for server_id in servers:
-            state = self._states[server_id]
-            if state.notified:
+            state = self._states.get(server_id)
+            if not state:
                 continue
 
-            state.notified = True
+            if not is_timeout:
+                if state.notified:
+                    continue
+                state.notified = True
 
-            state.get_mem_usage()
+            state.get_mem_usage(server_id, self._mem_logger)
             state.compute_next_wake_up(sleep_min=self._sleep_min, sleep_max=self._sleep_max)
 
-    def _do_wait(self):
+    def _do_wait(self) -> List[int]:
         min_wake_up = self._find_min_wake_up()
 
-        now = time.time()
-        timeout = min_wake_up - now
-        print("TIMEOUT:", timeout, datetime.datetime.now())
-        if timeout < 0:
-            timeout = 0
+        timeout = min_wake_up - time.time()
+        if timeout > 0:
+            self._cond.wait(timeout=timeout)
 
-        self._cond.wait(timeout=timeout)
-        self._clear_notified()
+        return self._notify_timeout()
 
     def _run(self):
         while True:
+            timeout_servers: List[int] = []
             with self._mut:
-                while len(self._notified) == 0 and not self._closed:
-                    self._do_wait()
+                while len(self._notified) == 0 and len(timeout_servers) == 0 and not self._closed:
+                    timeout_servers = self._do_wait()
 
                 if self._closed:
                     self._finished.release()
@@ -137,7 +154,8 @@ class ServerStats:
                 notify_list = [server_id for server_id in self._notified]
                 self._notified.clear()
 
-            self._notify_servers(notify_list)
+            self._notify_servers(timeout_servers, True)
+            self._notify_servers(notify_list, False)
 
     def get_mem_usage(self, server_id: int) -> Optional[float]:
         return self._states[server_id].mem
--- test/proxy/test_stats.py
+++ test/proxy/test_stats.py
@@ -1,3 +1,4 @@
+import datetime
 import time
 import unittest
 
@@ -6,32 +7,51 @@ import redis
 from memproxy.proxy import ServerStats
 
 
+def mem_logger(server_id: int, mem: float):
+    print("USAGE =", server_id, mem, datetime.datetime.now())
+
+
 class TestServerStatsSingleRedis(unittest.TestCase):
+    stats: ServerStats
+
     def setUp(self) -> None:
         self.redis = redis.Redis()
         self.redis.flushall()
         self.redis.script_flush()
 
-        duration = 100
+        duration = 7
 
         self.stats = ServerStats(clients={
             21: self.redis,
-        }, sleep_min=duration, sleep_max=duration)
+            22: self.redis,
+        }, sleep_min=duration, sleep_max=duration,
+            mem_logger=mem_logger,
+        )
 
         self.addCleanup(self.stats.shutdown)
 
+    def test_simple(self) -> None:
+        self.stats.notify_server_failed(21)
+        self.stats.notify_server_failed(22)
+        time.sleep(0.1)
+
     def test_normal(self) -> None:
         self.skipTest('not running')
-
-        v = self.stats.get_mem_usage(21)
-
+        print("START", datetime.datetime.now())
         time.sleep(3)
 
+        self.stats.notify_server_failed(21)
+        self.stats.notify_server_failed(22)
+        self.stats.notify_server_failed(23)
+
+        time.sleep(1)
+        self.stats.notify_server_failed(21)
+        self.stats.notify_server_failed(22)
+
+        v = self.stats.get_mem_usage(21)
+        print("MEMORY:", v)
+
+        time.sleep(15)
         self.stats.notify_server_failed(21)
         time.sleep(1)
         self.stats.notify_server_failed(21)
-
-        v = self.stats.get_mem_usage(21)
-        print(v)
-
-        time.sleep(3)
